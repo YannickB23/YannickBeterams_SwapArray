4105347438385241b38bce92776abb7ee8e0ee2e
YANNICK BETERAMS <YANNICK.BETERAMS@student.intecbrussel.be>
2020-11-19 09:34:59 +0100

    Add project files.

---
+++ OefeningSwapArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningSwapArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine(string.Join(",", array1));
+
+            int x = 0;
+            int z = array1.Length - 1;
+            while (x < z)
+            {
+                int reverse = array1[x];
+                array1[x] = array1[z];
+                array1[z] = reverse;
+                x++;
+                z--;
+            }
+            Console.WriteLine(string.Join(",", array1));
+        }
+    }
+}
